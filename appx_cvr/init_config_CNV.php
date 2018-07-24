55af748202361a0b4b09ce7951ab311d71b96510
edumu <edumu@ubuntu>
2018-07-24 14:30:13 -0500

    24/07/2018

--- appx_cvr/init_config_CNV.php
+++ appx_cvr/init_config_CNV.php
@@ -1,11 +1,8 @@
 <?php
 
-define("CNV_BASE_URL", 'http://localhost/convivere/');
+/**********    local url *************/
+define("CNV_BASE_URL_TEST", 'http://localhost/convivere/');
 
 
-///**BD**////
-
-define("CNV_HOSTNAME", 'localhost');
-define("CNV_USERNAME", 'convivere_admin');
-define("CNV_PWD"     , 'c0nv1v3r3');
-define("CNV_DB"      , 'convivere');
+/**********    PRODUCCION URL *************/
+define("CNV_BASE_URL_PROD", 'http://convivere.mx');
