e3cf11fba24ba25bf9f8d197cb5acb64448c2322
edumu <edumu@ubuntu>
2018-07-24 22:42:50 -0500

    24/07/2018

--- appx_cvr/init_config_CNV.php
+++ appx_cvr/init_config_CNV.php
@@ -5,5 +5,6 @@ define("CNV_BASE_URL_TEST", 'http://localhost/convivere/');
 
 
 /**********    PRODUCCION URL *************
-define("CNV_BASE_URL_PROD", 'https://convivere.azurewebsites.net/');*/
-define("CNV_BASE_URL_PROD", 'http://convivere.mx');
+define("CNV_BASE_URL_PROD", 'http://convivere.mx');*/
+define("CNV_BASE_URL_PROD", 'https://convivere.azurewebsites.net/');
+
