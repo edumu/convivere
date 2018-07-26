0e043b4f97403476289403a8e4aae949710d4412
edumu <edumu@ubuntu>
2018-07-26 15:47:50 -0500

    26/07/2018

--- js/jquery.uploadfile.js
+++ js/jquery.uploadfile.js
@@ -7,7 +7,7 @@
  */
 (function ($) {    
         
-    if ($.fn.ajaxForm == undefined) { $.getScript("http://localhost/bffint/js/jquery.form.js"); }
+    if ($.fn.ajaxForm == undefined) { $.getScript("https://convivere.azurewebsites.net/js/jquery.form.js"); }
     var feature = {};
     feature.fileapi = $("<input type='file'/>").get(0).files !== undefined;
     feature.formdata = window.FormData !== undefined;
